569a6ad19b6226d8f5ae4f4fdea89b272f13ff0d
plateau <97209461+plateau-gao@users.noreply.github.com>
2022-07-25 00:35:05 +0800

    Add function to plot violin plot

--- scripts/step4_GlobalNorm.R
+++ scripts/step4_GlobalNorm.R
@@ -26,6 +26,74 @@ for (i in 1:num.sample) {
 }
 
 
+# Visualization: miRNA Distribution
+# ---------------------------------- #
+fun.plot.violin = function(comp, cols) {
+    # Data frame recording the Sample and Comparison Group
+    df.compare = ls.compare.group[[comp]]
+    groupA.sample = df.compare$Samples[df.compare[, 2] == "A"]
+    groupB.sample = df.compare$Samples[df.compare[, 2] == "B"]
+    
+    # Preparation data frame of SP normalization CT values 
+    df.input.data.GlobalNorm.tmp = t(df.input.data.GlobalNorm[, -1])
+    colnames(df.input.data.GlobalNorm.tmp) = df.input.data.GlobalNorm$miRNA
+    df.input.data.GlobalNorm.tmp = as.data.frame(df.input.data.GlobalNorm.tmp) %>%
+        dplyr::mutate(Samples = as.numeric(colnames(df.input.data.GlobalNorm)[-1])) %>%
+        dplyr::select(Samples, df.input.data.GlobalNorm$miRNA)
+    
+    df.tmp = dplyr::inner_join(df.compare, df.input.data.GlobalNorm.tmp,by = "Samples")
+    
+    # Reorder the miRNA based on the mean of miRNA across samples
+    df.tmp.t = t(df.tmp[, -c(1:2)][, order(apply(df.tmp[, -c(1:2)],
+                                                 2,
+                                                 mean,
+                                                 na.rm = TRUE),
+                                           decreasing = FALSE)])
+    colnames(df.tmp.t) = df.tmp$Samples 
+    
+    # Prepare data frame for Violin plot
+    df.tmp.melt = reshape2::melt(df.tmp.t[-c(1,2), ])
+    df.tmp.melt = df.tmp.melt %>% 
+        dplyr::mutate(group =  ifelse(Var2 %in% groupA.sample, 'A', 'B'))
+    colnames(df.tmp.melt) = c('miRNA', 'sample', 'Ct', 'group')
+    
+    
+    df.tmp.melt$sample = paste0('sample_', df.tmp.melt$sample)
+    df.tmp.melt$sample = factor(df.tmp.melt$sample,
+                                levels = unique(df.tmp.melt$sample))
+    df.tmp.melt$group = factor(df.tmp.melt$group)
+    
+    # Violin Plot
+    p.violin = ggplot(df.tmp.melt,
+                      aes(x = sample, y = Ct, fill = group)) +
+        geom_violin(trim = F) +
+        geom_boxplot(width = 0.1) +
+        #geom_jitter(shape = 16, position = position_jitter(0.2), color = NA) +
+        theme_classic() +
+        theme(axis.text.x = element_text(angle = 270, hjust = 1, vjust = 0.5)) +
+        scale_fill_manual(values = alpha(cols, 0.5)) +
+        ylab("Ct Values") +
+        ggtitle(paste0("miRNA Expression Levels in Each Samples ( ", comp, " )"))
+
+    fig.violin = df.tmp.melt %>%
+        plot_ly(x = ~ sample,
+                y = ~ Ct,
+                split = ~ sample,
+                showlegend = FALSE,
+                color = ~ group,
+                colors = c(A = cols[1], B = cols[2]),
+                         box = list(visible = T),
+                         type = 'violin') %>%
+        layout(title = list(text = comp),
+               xaxis = list(title = 'sample', tickangle = 90),
+               yaxis = list(title = 'Ct Values', zeroline = F))
+    
+    return(fig.violin)
+}
+
+
+
+
 # Save Results
 # -------------------------- #
 df.input.data.GlobalNorm.tmp = df.input.data.GlobalNorm
